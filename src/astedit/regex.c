6f82bd5ce61ebfed883e5ddbf5ccf4e29c6ac436
Jens Stimpfle <jfs@jstimpfle.de>
2019-12-11 13:20:25 +0100

    save some more working state on regex.c

--- src/astedit/regex.c
+++ src/astedit/regex.c
@@ -17,43 +17,35 @@ enum {
         QUANTIFIER_NONE_OR_MORE,
 };
 
-struct AnyCharacterNode {
-        char dummy;
-};
-
 struct SpecificCharacterNode {
         int character;
 };
 
-struct EmptyRegexNode {
-        char dummy;
-};
-
 struct SplitRegexNode {
-        struct RegexNode *other;
+        int otherIndex;
 };
 
 struct RegexNode {
-        struct RegexNode *next;
+        int nextIndex;
         int nodeKind;
         union {
-                struct AnyCharacterNode tAnyCharacter;
                 struct SpecificCharacterNode tSpecificCharacter;
-                struct EmptyRegexNode tEmpty;
                 struct SplitRegexNode tSplit;
         } data;
 };
 
 struct RegexReadGroupCtx {
         /* a REGEXNODE_EMPTY node that allows us to add new alternatives */
-        struct RegexNode *entryNode;
+        int entryNodeIndex;
         /* a pre-allocated exit node. This will be used later for the first
          * element following the group, so it's not yet decided what kind
          * of node this is. */
-        struct RegexNode *exitNode;
+        int exitNodeIndex;
 };
 
 struct RegexReadCtx {
+        int bad;  // bad flag
+
         const char *pattern;
         int patternLength;
         int currentCharIndex;
@@ -61,13 +53,19 @@ struct RegexReadCtx {
         struct RegexReadGroupCtx *groupStack;
         int groupStackSize;
 
-        struct RegexNode *lastNode;
+        struct RegexNode *nodes;
+        int numNodes;
+
+        int initialNodeIndex;
+        int lastNodeIndex;  // current last node, where new nodes get appended
 };
 
 static void _fatal_regex_parse_error_fv(struct LogInfo logInfo,
                                         struct RegexReadCtx *ctx,
                                         const char *fmt, va_list ap)
 {
+        ctx->bad = 1;
+
         _log_begin(logInfo);
         log_writef("ERROR In pattern at char #%d: ", ctx->currentCharIndex);
         log_writefv(fmt, ap);
@@ -88,21 +86,20 @@ static void _fatal_regex_parse_error(struct LogInfo logInfo,
 #define fatal_regex_parse_error_fv(ctx, fmt, ap) _fatal_regex_parse_error_fv(MAKE_LOGINFO(), (ctx), (fmt), (ap))
 #define fatal_regex_parse_error(ctx, fmt, ...) _fatal_regex_parse_error(MAKE_LOGINFO(), (ctx), (fmt), ##__VA_ARGS__)
 
-static struct RegexNode *alloc_new_node(struct RegexReadCtx *ctx)
+static int alloc_new_node(struct RegexReadCtx *ctx)
 {
-        UNUSED(ctx);
-        struct RegexNode *out;
-        ALLOC_MEMORY(&out, 1);
-        out->next = NULL;
-        return out;
+        int nodeIndex = ctx->numNodes ++;
+        REALLOC_MEMORY(&ctx->nodes, ctx->numNodes);
+        ctx->nodes[nodeIndex].nextIndex = -1;
+        return nodeIndex;
 }
 
-static void append_node(struct RegexReadCtx *ctx, struct RegexNode *node)
+static void append_node(struct RegexReadCtx *ctx, int nodeIndex)
 {
-        ENSURE(node->next == NULL);
-        ENSURE(ctx->lastNode->next == NULL);
-        ctx->lastNode->next = node;
-        ctx->lastNode = node;
+        ENSURE(ctx->nodes[nodeIndex].nextIndex == -1);
+        ENSURE(ctx->nodes[ctx->lastNodeIndex].nextIndex == -1);
+        ctx->nodes[ctx->lastNodeIndex].nextIndex = nodeIndex;
+        ctx->lastNodeIndex = nodeIndex;
 }
 
 static int next_pattern_char(struct RegexReadCtx *ctx)
@@ -132,10 +129,10 @@ static int finish_regex(struct RegexReadCtx *ctx)
 static void finish_current_alternative(struct RegexReadCtx *ctx)
 {
         ENSURE(ctx->groupStackSize > 0);
-        struct RegexNode *entryNode = ctx->groupStack[ctx->groupStackSize-1].entryNode;
-        struct RegexNode *exitNode = ctx->groupStack[ctx->groupStackSize-1].exitNode;
-        append_node(ctx, exitNode);  // to finish current group, append exitNode
-        ctx->lastNode = entryNode;
+        int entryNodeIndex = ctx->groupStack[ctx->groupStackSize-1].entryNodeIndex;
+        int exitNodeIndex = ctx->groupStack[ctx->groupStackSize-1].exitNodeIndex;
+        append_node(ctx, exitNodeIndex);  // to finish current group, append exitNode
+        ctx->lastNodeIndex = entryNodeIndex;
 }
 
 static void start_new_alternative(struct RegexReadCtx *ctx)
@@ -146,30 +143,31 @@ static void start_new_alternative(struct RegexReadCtx *ctx)
         finish_current_alternative(ctx);
         // insert a split
         ENSURE(ctx->groupStackSize > 0);
-        struct RegexNode *entryNode = ctx->groupStack[ctx->groupStackSize-1].entryNode;
-        ENSURE(entryNode == ctx->lastNode);
-        struct RegexNode *splitNode = alloc_new_node(ctx);
-        splitNode->nodeKind = REGEXNODE_SPLIT;
-        splitNode->data.tSplit.other = entryNode->next;
-        entryNode->next = splitNode;
-        ctx->lastNode = splitNode;
+        int entryNodeIndex = ctx->groupStack[ctx->groupStackSize-1].entryNodeIndex;
+        ENSURE(entryNodeIndex == ctx->lastNodeIndex);
+        int splitNodeIndex = alloc_new_node(ctx);
+        ctx->nodes[splitNodeIndex].nodeKind = REGEXNODE_SPLIT;
+        ctx->nodes[splitNodeIndex].data.tSplit.otherIndex = ctx->nodes[entryNodeIndex].nextIndex;
+        ctx->nodes[entryNodeIndex].nextIndex = splitNodeIndex;
+        ctx->lastNodeIndex = splitNodeIndex;
 }
 
 static void push_group(struct RegexReadCtx *ctx)
 {
-        struct RegexNode *entryNode = alloc_new_node(ctx);
-        struct RegexNode *exitNode = alloc_new_node(ctx);
+        /* This function also needs to work when ctx->groupStackSize == 0,
+         * since it is called in setup_readctx(). Otherwise, we can expect
+         * to have ctx->groupStackSize > 0. */
+        int entryNodeIndex = alloc_new_node(ctx);
+        int exitNodeIndex = alloc_new_node(ctx);
+        ctx->nodes[entryNodeIndex].nodeKind = REGEXNODE_EMPTY;
+        ctx->nodes[exitNodeIndex].nodeKind = REGEXNODE_EMPTY;
 
-        entryNode->nodeKind = REGEXNODE_EMPTY;
-        exitNode->nodeKind = REGEXNODE_EMPTY;
+        append_node(ctx, entryNodeIndex);
 
-        append_node(ctx, entryNode);
-
-        ENSURE(ctx->groupStackSize > 0);
         int g = ctx->groupStackSize ++;
         REALLOC_MEMORY(&ctx->groupStack, ctx->groupStackSize);
-        ctx->groupStack[g].entryNode = entryNode;
-        ctx->groupStack[g].exitNode = exitNode;
+        ctx->groupStack[g].entryNodeIndex = entryNodeIndex;
+        ctx->groupStack[g].exitNodeIndex = exitNodeIndex;
 }
 
 static int pop_group(struct RegexReadCtx *ctx)
@@ -179,7 +177,7 @@ static int pop_group(struct RegexReadCtx *ctx)
                 return 0;
         finish_current_alternative(ctx);
         ENSURE(ctx->groupStackSize > 0);
-        ctx->lastNode = ctx->groupStack[ctx->groupStackSize - 1].exitNode;
+        ctx->lastNodeIndex = ctx->groupStack[ctx->groupStackSize - 1].exitNodeIndex;
         ctx->groupStackSize --;
         return 1;
 }
@@ -192,6 +190,8 @@ static int add_quantifier(struct RegexReadCtx *ctx, int quantifierKind)
 
 static void setup_readctx(struct RegexReadCtx *ctx, const char *pattern)
 {
+        ctx->bad = 0;
+
         ctx->pattern = pattern;
         ctx->patternLength = strlen(pattern);
         ctx->currentCharIndex = 0;
@@ -199,12 +199,17 @@ static void setup_readctx(struct RegexReadCtx *ctx, const char *pattern)
         ctx->groupStack = NULL;
         ctx->groupStackSize = 0;
 
-        ctx->groupStackSize = 1;
-        ALLOC_MEMORY(&ctx->groupStack, ctx->groupStackSize);
-        ctx->groupStack[0].entryNode = alloc_new_node(ctx);
-        ctx->groupStack[0].exitNode = alloc_new_node(ctx);
+        ctx->nodes = NULL;
+        ctx->numNodes = 0;
+        ctx->lastNodeIndex = -1;
 
-        ctx->lastNode = ctx->groupStack[0].entryNode;
+        int initialNodeIndex = alloc_new_node(ctx);
+        ctx->nodes[initialNodeIndex].nodeKind = REGEXNODE_EMPTY;
+        ctx->initialNodeIndex = initialNodeIndex;
+        ctx->lastNodeIndex = initialNodeIndex;
+
+        // add a base group that is always there
+        push_group(ctx);
 }
 
 static void teardown_readctx(struct RegexReadCtx *ctx)
@@ -221,10 +226,10 @@ static int is_normal_character(int c)
 
 static void add_character_node(struct RegexReadCtx *ctx, int character)
 {
-        struct RegexNode *node = alloc_new_node(ctx);
-        node->nodeKind = REGEXNODE_SPECIFIC_CHARACTER;
-        node->data.tSpecificCharacter.character = character;
-        append_node(ctx, node);
+        int nodeIndex = alloc_new_node(ctx);
+        ctx->nodes[nodeIndex].nodeKind = REGEXNODE_SPECIFIC_CHARACTER;
+        ctx->nodes[nodeIndex].data.tSpecificCharacter.character = character;
+        append_node(ctx, nodeIndex);
 }
 
 static void read_pattern(struct RegexReadCtx *ctx)
@@ -304,10 +309,102 @@ readmore:;
         goto readmore;
 }
 
-static int match_regex(struct RegexNode *entryNode, const char *string, int length)
+
+struct MatchCtx {
+        struct RegexNode *nodes;
+        int numNodes;
+
+        int *isActive;
+        int *nextIsActive;
+};
+
+static void set_active(struct MatchCtx *ctx, int nodeIndex)
 {
-        // TODO
-        return 0;
+        if (ctx->nodes[nodeIndex].nodeKind == REGEXNODE_SPLIT) {
+                set_active(ctx, ctx->nodes[nodeIndex].nextIndex);
+                set_active(ctx, ctx->nodes[nodeIndex].data.tSplit.otherIndex);
+        }
+        else if (ctx->nodes[nodeIndex].nodeKind == REGEXNODE_EMPTY) {
+                set_active(ctx, ctx->nodes[nodeIndex].nextIndex);
+        }
+        else {
+                ctx->nextIsActive[nodeIndex] = 1;
+        }
+}
+
+static void set_successors_active(struct MatchCtx *ctx, int nodeIndex)
+{
+        ENSURE(0 <= nodeIndex && nodeIndex < ctx->numNodes);
+        ENSURE(ctx->nodes[nodeIndex].nodeKind != REGEXNODE_SPLIT);
+        ENSURE(ctx->nodes[nodeIndex].nodeKind != REGEXNODE_EMPTY);
+
+        int nextNodeIndex = ctx->nodes[nodeIndex].nextIndex;
+        set_active(ctx, nextNodeIndex);
+}
+
+static void prepare_matching_for_next_char(struct MatchCtx *ctx)
+{
+        int *tmp = ctx->isActive;
+        ctx->isActive = ctx->nextIsActive;
+        ctx->nextIsActive = tmp;
+        for (int i = 0; i < ctx->numNodes; i++)
+                ctx->nextIsActive[i] = 0;
+}
+
+static void setup_matchctx_from_readctx(struct MatchCtx *matchCtx,
+                                        struct RegexReadCtx *readCtx)
+{
+        matchCtx->nodes = readCtx->nodes;
+        matchCtx->numNodes = readCtx->numNodes;
+        ALLOC_MEMORY(&matchCtx->isActive, matchCtx->numNodes);
+        ALLOC_MEMORY(&matchCtx->nextIsActive, matchCtx->numNodes);
+        for (int i = 0; i < matchCtx->numNodes; i++)
+                matchCtx->nextIsActive[i] = 0;
+        ENSURE(matchCtx->nodes[readCtx->initialNodeIndex].nodeKind == REGEXNODE_EMPTY);
+        set_active(matchCtx, matchCtx->nodes[readCtx->initialNodeIndex].nextIndex);
+        prepare_matching_for_next_char(matchCtx);
+}
+
+static void release_matchctx_after_setup_from_readctx(struct MatchCtx *matchCtx)
+{
+        FREE_MEMORY(&matchCtx->isActive);
+        FREE_MEMORY(&matchCtx->nextIsActive);
+}
+
+static int match_regex(struct RegexReadCtx *readCtx, const char *string, int length)
+{
+        struct MatchCtx matchCtx;
+        struct MatchCtx *const ctx = &matchCtx;
+        setup_matchctx_from_readctx(&matchCtx, readCtx);
+
+        for (int i = 0;
+             i < length;
+             i++, prepare_matching_for_next_char(ctx))
+        {
+                int c = string[i];
+                log_postf("now at character '%c'", c);
+                for (int nodeIndex = 0; nodeIndex < ctx->numNodes; nodeIndex++) {
+                        if (!ctx->isActive[nodeIndex])
+                                continue;
+                        struct RegexNode *node = &ctx->nodes[nodeIndex];
+                        switch (node->nodeKind) {
+                        case REGEXNODE_SPECIFIC_CHARACTER:
+                                if (c == node->data.tSpecificCharacter.character) {
+                                        log_postf("match specific character.");
+                                        set_successors_active(ctx, nodeIndex);
+                                }
+                                break;
+                        case REGEXNODE_ANY_CHARACTER:
+                                log_postf("match any character.");
+                                set_successors_active(ctx, nodeIndex);
+                                break;
+                        }
+                }
+        }
+
+        release_matchctx_after_setup_from_readctx(ctx);//XXX
+
+        return 1;//XXX
 }
 
 int main(int argc, const char **argv)
@@ -320,6 +417,10 @@ int main(int argc, const char **argv)
                 struct RegexReadCtx *ctx = &readCtx;
                 setup_readctx(ctx, pattern);
                 read_pattern(ctx);
+                if (ctx->bad)
+                        log_postf("abort due to pattern read error.");
+                else
+                        match_regex(ctx, "foobar", sizeof "foobar" - 1);
                 teardown_readctx(ctx);
         }
         return 0;
