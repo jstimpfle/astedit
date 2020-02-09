46c88ef3f520d7da6114968d251e5b25d985e72a
Jens Stimpfle <jfs@jstimpfle.de>
2020-02-09 18:56:34 +0100

    fix issue because sound on Windows is still missing

--- src/astedit/sound-win32.c
+++ src/astedit/sound-win32.c
@@ -16,7 +16,10 @@ void update_available_samples(void)
 
 int get_available_samples(void)
 {
-        return 0;
+        // as long as there is no sound implementation, report
+        // a number > 0 as the number of available bytes. This
+        // is so that writers don't get stuck re-trying.
+        return 4096;
 }
 
 void write_samples(const uint16_t *samples, int nsamples)
